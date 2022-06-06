dbad780c18f1ce4bbf5c6b5ef56af53f3c531822
fangjian <fangjian@droi.com>
2022-06-06 15:20:45 +0800

    update openAI project structure

--- JavaCamp/Sort.java
+++ JavaCamp/Sort.java
@@ -30,7 +30,7 @@ public class Sort{
         }
     }
 
-    void heapAdjust(int[] arr, int i, int length){
+    public static void heapAdjust(int[] arr, int i, int length){
         int temp = arr[i];
         int child = 2 * i + 1;
         while(child < length){
@@ -45,5 +45,24 @@ public class Sort{
             child = 2 * i + 1;
         }
         arr[i] = temp;
+    }
+
+    //选择排序
+    public static void selectSort(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            int min = i;
+            for(int j = i + 1; j < arr.length; j++){
+                if(arr[j] < arr[min]){
+                    min = j;
+                }
+            }
+            swap(arr, i, min);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
